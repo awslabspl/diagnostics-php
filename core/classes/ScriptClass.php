bdd9ffd83a6f4ac8fcacbae0cde2ea764e1d6183
awslabs.pl <diagnostics@interia.eu>
2021-02-03 09:51:57 +0100

    ScriptClass.php

---
+++ core/classes/ScriptClass.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace diagnosticsphp\core;
+
+
+use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
+
+abstract class ScriptClass implements ScriptInfoInterface, LoggerInterface
+{
+
+    private $scn;
+    private $sfn;
+
+    public function getScriptName()
+    {
+        $this->scn = $_SERVER['SCRIPT_NAME'];
+        if (isset($this->scn)){
+            echo $this->scn;
+        } else {
+            echo $this->scn;
+            $this->log(LogLevel::NOTICE, $this->scn);
+        }
+    }
+
+    public function getScriptFileName()
+    {
+        $this->sfn = $_SERVER['SCRIPT_FILENAME'];
+        if (isset($this->sfn)){
+            echo $this->sfn;
+        } else {
+            echo $this->sfn;
+            $this->log(LogLevel::NOTICE, $this->sfn);
+        }
+    }
+}

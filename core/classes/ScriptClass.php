51a562454f36630a6c41a186d58bdba853ae8e60
awslabs.pl <diagnostics@interia.eu>
2021-02-04 15:00:57 +0100

    comments (#3)

--- core/classes/ScriptClass.php
+++ core/classes/ScriptClass.php
@@ -7,12 +7,19 @@ namespace diagnosticsphp\core\scriptClass;
 use Psr\Log\LoggerInterface;
 use Psr\Log\LogLevel;
 
+/**
+ * Class ScriptClass
+ * @package diagnosticsphp\core\scriptClass
+ */
 abstract class ScriptClass implements ScriptInfoInterface, LoggerInterface
 {
 
     private $scn;
     private $sfn;
 
+    /**
+     * @since 0.1-pre
+     */
     public function getScriptName()
     {
         $this->scn = $_SERVER['SCRIPT_NAME'];
@@ -24,6 +31,9 @@ abstract class ScriptClass implements ScriptInfoInterface, LoggerInterface
         }
     }
 
+    /**
+     * @since 0.1-pre
+     */
     public function getScriptFileName()
     {
         $this->sfn = $_SERVER['SCRIPT_FILENAME'];

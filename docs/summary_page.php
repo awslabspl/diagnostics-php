8eaeaef5afaa774356460cea7d344a1d843c8aa1
awslabs.pl <diagnostics@interia.eu>
2021-02-07 17:57:55 +0100

    summary page draft

---
+++ docs/summary_page.php
@@ -0,0 +1,20 @@
+<html lang="en">
+<head>
+    <meta spellcheck="false">
+    <meta http-equiv="x-ua-compatible" content="*/*">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="resources/styles/index.css" type="text/css"/>
+    <link rel="stylesheet" href="resources/styles/bootstrap.css" type="text/css">
+    <link rel="stylesheet" href="resources/styles/tabbedIndex.scss" as="style" type="text/css"/>
+    <title>Diagnostics || Summary</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
+<?php
+?>

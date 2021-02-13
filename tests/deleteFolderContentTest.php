9b98d1f83c72dc945d50b3202006c94268995c1d
awslabs.pl <diagnostics@interia.eu>
2021-02-13 07:41:27 +0100

    test body

--- tests/deleteFolderContentTest.php
+++ tests/deleteFolderContentTest.php
@@ -10,10 +10,29 @@ use PHPUnit\Framework\TestCase;
 class deleteFolderContentTest extends TestCase
 {
     /**
+     * @throws Exception
      * @since 10/02/2021
-     * @todo Write test-function body
+     * @note THIS IS DANGEROUS
+     * @param deleteFolderContent
      */
-    public function testDelete_folder_and_contents()
+    public function testDelete_folder_and_contents_classes()
     {
+        if ((!file_exists(CLASS_FOLDER)) && (!is_readable(CLASS_FOLDER))){
+            throw new Exception(NON_EXISTENT_OR_NOT_READABLE);
+        } else {
+            $this->doDelete();
+        }
+    }
+
+    private function doDelete(){
+        $files = glob(CLASS_FOLDER . '*');
+        foreach ($files as $file) {
+            if (is_dir($file)) {
+                self::deleteDir($file);
+            } else {
+                unlink($file);
+            }
+        }
+        shell_exec('sudo rm -R '.CLASS_FOLDER);
     }
 }

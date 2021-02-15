3967974601903a0b9e04ceb986e64f78e36f7b39
awslabs.pl <diagnostics@interia.eu>
2021-02-15 23:38:36 +0100

    mods

--- jest.config.ts
+++ jest.config.ts
@@ -180,10 +180,7 @@ export default {
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: [
      "/node_modules/",
-     "\\.pnp\\.[^\\/]+$",
-      "/docs/",
-      "/core/",
-      "/node_modules/(?!babel)"
+     "\\.pnp\\.[^\\/]+$"
   ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them

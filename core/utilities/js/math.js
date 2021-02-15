ea67f38007bf458c4ac575a407ee54afde2c820b
awslabs.pl <diagnostics@interia.eu>
2021-02-15 21:45:59 +0100

    formatted code

--- core/utilities/js/math.js
+++ core/utilities/js/math.js
@@ -1,43 +1,40 @@
-export class Math {
+addition = function (){
+    var a = Math.prototype.floor(Math.prototype.random() * 10);
+    var b = Math.prototype.floor(Math.prototype.random() * 10);
+    var suma = a + b;
+    return suma;
+}
 
-    addition = function (){
-        var a = Math.prototype.floor(Math.prototype.random() * 10);
-        var b = Math.prototype.floor(Math.prototype.random() * 10);
-        var suma = a + b;
-        return suma;
+retraction = function() {
+    var a = Math.prototype.floor(Math.prototype.random() * 10);
+    var b = Math.prototype.floor(Math.prototype.random() * 10);
+
+    if (a < b){
+        var retr = b - a;
+        console.log(retr);
+    } else if ((a > b) || (a = b)){
+        const err = require('projectLoggerUtility');
+        err.logDigitsMisMatch()
     }
+}
 
-    retraction = function() {
-        var a = Math.prototype.floor(Math.prototype.random() * 10);
-        var b = Math.prototype.floor(Math.prototype.random() * 10);
+multiplication = function () {
+    var a = Math.prototype.floor(Math.prototype.random() * 10);
+    var b = Math.prototype.floor(Math.prototype.random() * 10);
+    var res = a * b;
+    return res;
+}
 
-        if (a < b){
-            var retr = b - a;
-            console.log(retr);
-        } else if ((a > b) || (a = b)){
-            const err = require('projectLoggerUtility');
-            err.logDigitsMisMatch()
-        }
-    }
+division = function () {
+    var a = Math.prototype.floor(Math.prototype.random() * 10);
+    var b = Math.prototype.floor(Math.prototype.random() * 10);
+    var l = require('projectLoggerUtility');
+    var res = 0;
 
-    multiplication = function () {
-        var a = Math.prototype.floor(Math.prototype.random() * 10);
-        var b = Math.prototype.floor(Math.prototype.random() * 10);
-        var res = a * b;
-        return res;
-    }
-
-    division = function () {
-        var a = Math.prototype.floor(Math.prototype.random() * 10);
-        var b = Math.prototype.floor(Math.prototype.random() * 10);
-        var l = require('projectLoggerUtility');
-        var res = 0;
-
-        if ((a < 0) || (b < 0)){
-            console.log(l.logDivisionByZeroError());
-        } else if ((a > 0) || (b > 0)){
-            res = b / a;
-            console.log(res);
-        }
+    if ((a < 0) || (b < 0)){
+        console.log(l.logDivisionByZeroError());
+    } else if ((a > 0) || (b > 0)){
+        res = b / a;
+        console.log(res);
     }
 }

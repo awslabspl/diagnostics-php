356c3d908573453a8a7c76c99849f6518e88d2a1
awslabs.pl <diagnostics@interia.eu>
2021-02-16 08:50:57 +0100

    new test

--- tests/js/server-tests.js
+++ tests/js/server-tests.js
@@ -3,3 +3,8 @@ import {withGlobal} from "../../docs/resources/js/fake-timers";
 test("expect to have UserAgent", ()=>{
     expect(withGlobal.userAgent).not.toBeEmpty;
 });
+
+/**
+ * @default true
+ * @summary Test wether maxPacketLife exceeds 300 ( ms )
+ */

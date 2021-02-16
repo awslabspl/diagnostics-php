ec090757a59e2d7a44ed72c2d930746dd66a9a56
awslabs.pl <diagnostics@interia.eu>
2021-02-16 09:48:13 +0100

    improved logger

--- Utils/js/projectLoggerUtility.js
+++ Utils/js/projectLoggerUtility.js
@@ -1,31 +1,31 @@
-export var logCiticalError = function(){
+var logCiticalError = function(){
     var severity = "Critical error";
     console.log("Level: "+severity);
 }
 
-export var logError = function(){
+var logError = function(){
     var severity = "Error";
     console.log("Level: "+severity);
 }
 
-export var logWarn = function(){
+var logWarn = function(){
     var severity = "Warning";
     console.log("Level: "+severity);
 }
 
-export var logInfo = function(){
+var logInfo = function(){
     var severity = "Info";
     console.log("Level: "+severity);
 }
 
 // Math.js specific messages
-export var logDigitsMisMatch = function(){
+var logDigitsMisMatch = function(){
     var severity = "Critical error";
     var msg = "A cannot be greater than B"
     console.log("Level: "+severity+" || "+msg);
 }
 
-export var logDivisionByZeroError = function(){
+var logDivisionByZeroError = function(){
     var severity = "Critical error";
     var msg = "Unable to divide by 0"
     console.log("Level: "+severity+" || "+msg);

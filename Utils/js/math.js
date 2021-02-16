671435c97d02af9668cfa1a9650b106b6f1294d6
awslabs.pl <diagnostics@interia.eu>
2021-02-16 13:02:28 +0100

    some more clarification

--- Utils/js/math.js
+++ Utils/js/math.js
@@ -1,15 +1,18 @@
 export var a = Math.random() * 10;
 export var b = Math.random() * 10;
 export var suma = 0;
+export var retr = 0;
+export var res = 0;
+export var divide = 0;
+
 
 export var addition = function (){
-    suma = a + b;
-    return suma;
+    return suma = a + b;
 }
 
 export var retraction = function() {
     if (a < b){
-        var retr = b - a;
+        retr = b - a;
         console.log(retr);
     } else if ((a > b) || (a = b)){
         const err = require('Utils/js/projectLoggerUtility');
@@ -18,18 +21,16 @@ export var retraction = function() {
 }
 
 export var multiplication = function () {
-    var res = a * b;
-    return res;
+    return res = a * b;
 }
 
 export var division = function () {
     var l = require('Utils/js/projectLoggerUtility');
-    var res = 0;
 
     if ((a < 0) || (b < 0)){
         console.log(l.logDivisionByZeroError());
     } else if ((a > 0) || (b > 0)){
-        res = b / a;
-        console.log(res);
+        divide = b / a;
+        console.log(divide);
     }
 }

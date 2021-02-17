58a44e57fe735652c5f46bbb1388412a5d4c4268
awslabs.pl <diagnostics@interia.eu>
2021-02-17 16:44:55 +0100

    php implementation

---
+++ Utils/php/sfs.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * Copyright (c) 2021.
+ */
+
+// setup the URL
+$url = 'http://api.stopforumspam.org/api';
+
+
+/**
+ *
+ */
+$data = array(
+        'name' => 'newspammer',
+        'email' => 'newspammer@this.baddomain.com',
+        'ip' => '185.61.149.193',
+        'host' => 'hostname'
+);
+
+$data = http_build_query(self::class);
+
+// init the request, set some info, send it and finally close it
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$result = curl_exec($ch);
+
+curl_close($ch);
+
+return $result;

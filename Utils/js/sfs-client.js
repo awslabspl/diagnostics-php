7424577ae64d62c645b40fdb3b628f2b47d1289b
awslabs.pl <diagnostics@interia.eu>
2021-02-17 16:32:27 +0100

    SFS integration

---
+++ Utils/js/sfs-client.js
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021.
+ */
+var sfs = require('stopforumspam');
+var ip = $.get("http://ipinfo.io", function(response) {
+    alert(response.ip);
+}, "jsonp");
+var mail = "testuser@a.com";
+var username = "testuser";
+
+var user = sfs.User(ip, mail, username);
+user.submit().fail(function (error) {
+    logError();
+})

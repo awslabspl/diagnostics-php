a1f74bd6e61bb89b989b6f5e93f53185da2741d1
awslabs.pl <diagnostics@interia.eu>
2021-02-19 11:13:09 +0100

    wrapped in describe block

--- tests/js/time-tests.js
+++ tests/js/time-tests.js
@@ -1,13 +1,15 @@
 import * as t from '../../Utils/js/generationtimes';
 
-test("page load time as function", ()=>{
-    expect(t.pageloadtime).toBeFunction;
-});
+describe("time series tests", ()=>{
+    test("page load time as function", ()=>{
+        expect(t.pageloadtime).toBeFunction;
+    });
 
-test("page load is a number", ()=>{
-    expect(t.pageloadtime).not.toBeNumber;
-});
+    test("page load is a number", ()=>{
+        expect(t.pageloadtime).not.toBeNumber;
+    });
 
-test("page load is not negative", ()=>{
-   expect(t.pageloadtime).toBeGreaterThan(0);
+    test("page load is not negative", ()=>{
+        expect(t.pageloadtime).toBeGreaterThan(0);
+    });
 });

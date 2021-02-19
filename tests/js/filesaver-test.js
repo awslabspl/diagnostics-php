5e9f449fbc57e5e0816074139de0219ea9a2b3a3
awslabs.pl <diagnostics@interia.eu>
2021-02-19 07:49:09 +0100

    tests

--- tests/js/filesaver-test.js
+++ tests/js/filesaver-test.js
@@ -2,3 +2,24 @@
  * All test files:: copyright (c) 2021. awslabspl
  ******************************************************************************/
 const fsv = require('../../Utils/js/file-saver');
+
+/**
+ * Exported functions ONLY
+ */
+describe("are functions defined", ()=>{
+   test("global function", ()=>{
+     expect(fsv.globalFunction).toBeDefined;
+   });
+
+   test("bom", ()=>{
+      expect(fsv.bom).toBeDefined;
+   });
+
+   test("download", ()=>{
+      expect(fsv.download).toBeDefined;
+   });
+
+   test("save", ()=>{
+       expect(fsv.saveAs).toBeDefined;
+   })
+});

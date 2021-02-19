6f394824b1a66627b23c2a4d1bd13441582d7aed
awslabs.pl <diagnostics@interia.eu>
2021-02-19 11:01:39 +0100

    fix string

--- Utils/js/generationtimes.js
+++ Utils/js/generationtimes.js
@@ -1,6 +1,6 @@
 const timerStart = Date.now();
 $(document).ready(function() {
-    console.log("Time until DOMready: ", Date.now()-timerStart);
+    console.log("Time until ready: ", Date.now()-timerStart);
 });
 var pageloadtime = $(window).load(function() {
     //console.log("Time until everything loaded: ", Date.now()-timerStart);

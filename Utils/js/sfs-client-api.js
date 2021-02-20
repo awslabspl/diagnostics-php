0e6aac7764d134067a26c8c22af3f4c8c4910662
awslabs.pl <diagnostics@interia.eu>
2021-02-20 16:19:21 +0100

    comment added

--- Utils/js/sfs-client-api.js
+++ Utils/js/sfs-client-api.js
@@ -3,6 +3,8 @@
  ******************************************************************************
  * @type {{config: {routes: [{path: string, name: string}, {path: string, name: string}], apiKey: string, searchParameters: [{searchAdd: string, name: string, submitAdd: string}, {searchAdd: string, name: string, submitAdd: string}, {searchAdd: string, name: string, submitAdd: string}], url: string}}}
  * @summary Uses stopforumspam.com's API
+ *
+ * Provided values are just exemplary. Please replace them with real ones BEFORE usage.
  */
 var sfs = require('stopforumspam');
 export var ip = "123.123.123.123";
